5cde4aaffafa22cfc13abbbf77e6727ba5d87cd5
James Mitchell <hello@jpmitchell.me>
2020-04-17 07:40:29 -0400

    add SDL window creation

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+void PollSDLEvents();
 
 int main(int argc, char** argv) {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  // init SDL2
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "Could not initialize SDL: " << SDL_GetError() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // create SDL window
+  SDL_Window *window = SDL_CreateWindow(
+    "Solar System",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    SDL_WINDOW_OPENGL
+  );
+
+  // check for window creation errors
+  if (window == NULL) {
+    std::cout << "Could not create SDL window: " << 
+      SDL_GetError() << std::endl;
+  }
+
+  // main program loop
+  bool programEnd = false;
+  SDL_Event e;
+  while (!programEnd) {
+    // poll SDL events
+    const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
+    while (SDL_PollEvent(&e) != 0) {
+      // terminate the program if the exit button or escape key is pressed
+      if (e.type == SDL_QUIT || keyboardState[SDL_SCANCODE_ESCAPE]) {
+        programEnd = true;
+      }
+    }
+  }
+
+  // cleanup
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return EXIT_SUCCESS;
 }
--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC=g++
 CFLAGS=-Wall -g
 NAME=SolarSystem
+LIBS=-lSDL2 -lGL
 
 main:
-	$(CC) $(CFLAGS) -o $(NAME) Main.cpp	
+	$(CC) $(CFLAGS) $(LIBS) -o $(NAME) Main.cpp	
